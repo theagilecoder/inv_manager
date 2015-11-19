7522a2cc0be79a603b37373d7623b2ecb53f918b
Pushkar Singh <theagilecoder@gmail.com>
2015-11-19 02:53:20 +0000

    Implement login logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root 'static_pages#spage1'
   get  'spage2' => 'static_pages#spage2'
   get 'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   resources :users
 end
