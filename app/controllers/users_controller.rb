3d63b80ee388aea4706d4088a7336e300a9016c0
Pushkar Singh <theagilecoder@gmail.com>
2015-12-09 08:36:36 +0000

    [minor] General Housekeeping

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
     if @user.save
       log_in @user
       flash[:success] = "Signed up successfully. Welcome to the Application!"
-      redirect_to @user
+      redirect_to home_path
     else
       render 'new'
     end
