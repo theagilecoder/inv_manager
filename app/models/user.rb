ee68e9e52a736356a24e356d345fda5675160ac7
Pushkar Singh <theagilecoder@gmail.com>
2015-12-12 03:44:09 +0000

    [minor] General Housekeeping

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,7 @@ class User < ActiveRecord::Base
   has_many :items, dependent: :destroy
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+  #validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  validates :password, presence: true, length: { minimum: 4 }, allow_nil: true
 end
